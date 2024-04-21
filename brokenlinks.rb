e226969e3ca86856f373078f9db78dd28c1faafb
Ewen Le Bihan <hey@ewen.works>
2024-04-21 15:17:58 +0200

    ✨ Improve brokenlinks.rb

--- brokenlinks.rb
+++ brokenlinks.rb
@@ -3,7 +3,13 @@ require "uri"
 require "net/http"
 require "json"
 require "set"
+require "yaml"
+
 require "bundler/inline"
+gemfile true do
+  source 'https://rubygems.org' 
+  gem 'parallel'
+end
 
 # dumb websites
 # maps hosts to allowed >=400 http status codes
@@ -12,18 +18,19 @@ exceptions = {
   "www.fiverr.com": 403,
   "distrokid.com": 403,
   "www.amazon.com": 500,
+  "evann.tech": 0,
 }
+
 
 check_every = 3600 / 8 # seconds
 
 
-gemfile true do
-  source 'https://rubygems.org' 
-  gem 'parallel'
+def log_error msg
+  File.write "deadlinks.log", "[#{Time.now}] #{msg}\n", mode: 'a+'
 end
 
-
 def parse_uri uri
+  if uri !~ /https?:\/\// then uri = "https://ewen.works#{uri}" end
   return URI.parse URI::Parser.new.escape uri
 end
 
@@ -40,18 +47,33 @@ def check_broken_links database, exceptions
     end
   end
 
+  YAML.load_file("./tags.yaml").each do |tag|
+    if tag.has_key? "learn more at"
+      links.add? parse_uri tag["learn more at"]
+    end
+  end
+
+  YAML.load_file("./technologies.yaml").each do |tag|
+    if tag.has_key? "learn more at"
+      links.add? parse_uri tag["learn more at"]
+    end
+  end
+
   return Parallel.map(links) do |url|
     begin
+      puts "Checking #{url}"
       response = (Net::HTTP.get_response url)
       if response.code.to_i >= 400
         unless exceptions.has_key? url.host.to_sym and response.code.to_i == exceptions[url.host.to_sym]
-          puts "#{url} broken: got status #{response.code}"
+          log_error "#{url} broken: got status #{response.code}"
           url
         end
       end
     rescue StandardError => ex
-      puts "#{url}: utterly broken: #{ex}"
-      url
+      unless exceptions.has_key? url.host.to_sym and exceptions[url.host.to_sym] == 0
+        log_error "#{url}: utterly broken: #{ex}"
+        url
+      end
     end
   end.compact
 end
@@ -60,7 +82,7 @@ def push_kuma_status error_messages
   uptime_kuma_push_url = URI.parse ARGV[1]
   push_params = {
     "status": if error_messages.empty? then "up" else "down" end,
-      "msg": if error_messages.empty? then "OK" else error_messages.join ", " end,
+      "msg": if error_messages.empty? then "OK" else error_messages.join " \n" end,
       "ping": ""
   }
 
